558781d63c70bea0f0d33057438fc829c80f1c25
noahhartmann <noahhartmann13@icloud.com>
2022-03-01 17:11:36 +0100

    Made SynthesizerPlayer a non static class and implemented constructor

--- JavaAudioSynthesizer/src/de/dhbw/java/main/SynthesizerPlayer.java
+++ JavaAudioSynthesizer/src/de/dhbw/java/main/SynthesizerPlayer.java
@@ -1,21 +1,30 @@
 package de.dhbw.java.main;
 
+import com.jsyn.JSyn;
 import com.jsyn.Synthesizer;
+import com.jsyn.unitgen.LineOut;
+import com.jsyn.unitgen.UnitVoice;
 
 public class SynthesizerPlayer
 {
-	private SynthesizerPlayer()
-	{
-		// Static class -> no need for constructor
-	}
+	private Synthesizer synthesizer;
+	private LineOut lineOut;
+	private UnitVoice unitVoice;
 
-	public static void playTone(int index)
+	public SynthesizerPlayer(UnitVoice unitVoice)
 	{
-		Synthesizer synth = SynthesizerHandler.getSynthesizer();
-		synth.start();
-		
-		// TODO
+		this.unitVoice = unitVoice;
+		this.lineOut = new LineOut();
+		this.synthesizer = JSyn.createSynthesizer();
 
-		synth.stop();
+		synthesizer.setRealTime(true);
+		synthesizer.add(unitVoice.getUnitGenerator());
+		synthesizer.add(lineOut);
+
+		this.unitVoice.getOutput().connect(0, lineOut.input, 0);
+		this.unitVoice.getOutput().connect(0, lineOut.input, 1);
+
+		synthesizer.start();
+		synthesizer.startUnit(lineOut);
 	}
 }
