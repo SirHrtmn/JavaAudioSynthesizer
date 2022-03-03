eec8827808f364cbc750f4277c98023b095d2369
noahhartmann <noahhartmann13@icloud.com>
2022-03-03 15:51:07 +0100

    Added envelope to sineCircuit & implemented noteOn/noteOff

--- JavaAudioSynthesizer/src/de/dhbw/java/main/synth/circuits/SineSynthCircuit.java
+++ JavaAudioSynthesizer/src/de/dhbw/java/main/synth/circuits/SineSynthCircuit.java
@@ -1,7 +1,8 @@
-package de.dhbw.java.main.circuits;
+package de.dhbw.java.main.synth.circuits;
 
 import com.jsyn.ports.UnitOutputPort;
 import com.jsyn.unitgen.Circuit;
+import com.jsyn.unitgen.EnvelopeDAHDSR;
 import com.jsyn.unitgen.FilterStateVariable;
 import com.jsyn.unitgen.SineOscillator;
 import com.jsyn.unitgen.UnitFilter;
@@ -13,16 +14,20 @@ public class SineSynthCircuit extends Circuit implements UnitVoice
 {
 	private UnitOscillator oscillator;
 	private UnitFilter filter;
+	private EnvelopeDAHDSR envelope;
 
 	public SineSynthCircuit()
 	{
 		super();
 		this.oscillator = new SineOscillator();
 		this.filter = new FilterStateVariable();
+		this.envelope = new EnvelopeDAHDSR();
 
+		super.add(envelope);
 		super.add(oscillator);
 		super.add(filter);
 
+		envelope.output.connect(oscillator.amplitude);
 		oscillator.output.connect(filter.input);
 	}
 
@@ -35,14 +40,14 @@ public class SineSynthCircuit extends Circuit implements UnitVoice
 	@Override
 	public void noteOff(TimeStamp stopTime)
 	{
-		// TODO Auto-generated method stub
-
+		envelope.input.set(0.0);
 	}
 
 	@Override
 	public void noteOn(double frequency, double amplitude, TimeStamp startTime)
 	{
-		// TODO Auto-generated method stub
-
+		oscillator.frequency.set(frequency);
+		
+		envelope.input.set(1.0);
 	}
 }
