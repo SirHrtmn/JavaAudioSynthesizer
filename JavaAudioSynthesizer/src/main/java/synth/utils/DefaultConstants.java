2373be358b3eccd4f97ffa311b2c9bad85032908
noahhartmann <noahhartmann13@icloud.com>
2022-03-16 11:14:01 +0100

    moved DefaultConstants and adapted init of configuration classes

--- JavaAudioSynthesizer/src/main/java/synth/utils/DefaultConstants.java
+++ JavaAudioSynthesizer/src/main/java/synth/utils/DefaultConstants.java
@@ -1,23 +1,22 @@
-package synth;
+package synth.utils;
 
 import synth.circuits.FilterEnvelopeVoice;
 import synth.circuits.VoiceCircuits;
 import synth.configuration.EnvelopeConfiguration;
 import synth.configuration.FilterConfiguration;
-import synth.configuration.FilterConfiguration.FilterType;
 
 public class DefaultConstants
-{	
+{
 	public static FilterConfiguration getFilterConfig()
 	{
-		return new FilterConfiguration(440.0, FilterType.BAND_PASS);
+		return new FilterConfiguration(0.0, 0.0, 0.0);
 	}
-	
+
 	public static EnvelopeConfiguration getEnvelopeConfig()
 	{
 		return new EnvelopeConfiguration(0.0, 0.2, 1.0, 1.0, 0.2);
 	}
-	
+
 	public static FilterEnvelopeVoice getVoice()
 	{
 		return VoiceCircuits.SinusVoice.getUnit();
