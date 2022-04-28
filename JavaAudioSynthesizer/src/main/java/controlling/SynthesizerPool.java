a890cc1118b5c354aa010a6afaf1364e631702e1
noahhartmann <noahhartmann13@icloud.com>
2022-04-28 12:53:03 +0200

    Implemented SynthesizerPool and SynthesizerConnection

---
+++ JavaAudioSynthesizer/src/main/java/controlling/SynthesizerPool.java
@@ -0,0 +1,123 @@
+package controlling;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+import configuration.EnvelopeConfiguration;
+import configuration.FilterConfiguration;
+import synthesis.ISynthesizer;
+import synthesis.OscillatorType;
+import synthesis.SynthesizerFactory;
+
+public class SynthesizerPool
+{
+	private static final int DEFAULT_SYNTHESIZER_COUNT = 5;
+
+	private static SynthesizerPool instance;
+
+	private List<SynthesizerConnection> freeSynths = new ArrayList<>();
+	private List<SynthesizerConnection> acquiredConnections = new ArrayList<>();
+
+	private OscillatorType oscillatorType;
+
+	private SynthesizerPool(OscillatorType oscillatorType)
+	{
+		this.oscillatorType = oscillatorType;
+		loadSynthesizers(DEFAULT_SYNTHESIZER_COUNT);
+	}
+
+	public static SynthesizerPool getInstance(OscillatorType oscillatorType)
+	{
+		if (instance == null)
+		{
+			instance = new SynthesizerPool(oscillatorType);
+		}
+
+		instance.setOscillatorType(oscillatorType);
+		return instance;
+	}
+
+	public SynthesizerConnection acquireSynthesizer()
+	{
+		if (!freeSynths.isEmpty())
+		{
+			freeSynths.add(createSynthConnection());
+			return acquireSynthesizer();
+		}
+
+		SynthesizerConnection synth = freeSynths.remove(0);
+		acquiredConnections.add(synth);
+		return synth;
+	}
+
+	public void releaseSynthesizer(SynthesizerConnection synthConnection)
+	{
+		if (!acquiredConnections.contains(synthConnection))
+		{
+			return;
+		}
+
+		if (!oscillatorType.equals(synthConnection.getOscillatorType()))
+		{
+			acquiredConnections.remove(synthConnection);
+		}
+
+		acquiredConnections.remove(synthConnection);
+		freeSynths.add(synthConnection);
+	}
+
+	public void setOscillatorType(OscillatorType type)
+	{
+		if (oscillatorType.equals(type))
+		{
+			return;
+		}
+
+		this.oscillatorType = type;
+		applyForEachInList(conn -> conn.setSynthesizer(createSynthesizer()), freeSynths);
+		applyForEachInList(conn -> conn.setSynthesizer(createSynthesizer()), acquiredConnections);
+	}
+
+	public void applyFilterConfiguration(FilterConfiguration filterConfig)
+	{
+		Consumer<SynthesizerConnection> updateFilter = conn -> conn
+				.applyFilterConfiguration(filterConfig);
+
+		applyForEachInList(updateFilter, freeSynths);
+		applyForEachInList(updateFilter, acquiredConnections);
+	}
+
+	public void applyEnvelopeConfiguration(EnvelopeConfiguration envConfig)
+	{
+		Consumer<SynthesizerConnection> updateEnv = conn -> conn
+				.applyEnvelopeConfiguration(envConfig);
+
+		applyForEachInList(updateEnv, freeSynths);
+		applyForEachInList(updateEnv, acquiredConnections);
+	}
+
+	private void loadSynthesizers(int amount)
+	{
+		for (int i = 0; i < amount; i++)
+		{
+			freeSynths.add(createSynthConnection());
+		}
+	}
+
+	private SynthesizerConnection createSynthConnection()
+	{
+		return new SynthesizerConnection(createSynthesizer());
+	}
+
+	private ISynthesizer createSynthesizer()
+	{
+		return SynthesizerFactory.build(oscillatorType);
+	}
+
+	private void applyForEachInList(Consumer<SynthesizerConnection> action,
+			List<SynthesizerConnection> list)
+	{
+		list.forEach(action);
+	}
+}
