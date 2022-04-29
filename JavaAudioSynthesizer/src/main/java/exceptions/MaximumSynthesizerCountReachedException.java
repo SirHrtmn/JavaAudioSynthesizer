fc9caedec9ab92cd6556d0b9d80e97eec7509019
noahhartmann <noahhartmann13@icloud.com>
2022-04-29 17:43:26 +0200

    added serial in Max Synth Count Reached Exception

--- JavaAudioSynthesizer/src/main/java/exceptions/MaximumSynthesizerCountReachedException.java
+++ JavaAudioSynthesizer/src/main/java/exceptions/MaximumSynthesizerCountReachedException.java
@@ -2,6 +2,7 @@ package exceptions;
 
 public class MaximumSynthesizerCountReachedException extends RuntimeException
 {
+	private static final long serialVersionUID = -1861463443453581997L;
 
 	public MaximumSynthesizerCountReachedException()
 	{
